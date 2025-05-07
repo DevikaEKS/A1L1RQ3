80a3a830e8d9a14aa60f03c5034c7a57e2c5a602
Devika Satheesan <devikasatheesan489@gmail.com>
2025-05-07 13:45:24 +0530

    added itemcount

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,33 @@ import './App.css';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [count, setCount]  = useState(0);
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then(res => res.json())
-      .then(data => setProducts(data))
+      .then(data => {
+        setProducts(data);
+        setCount(data.length);             
+      })
       .catch(error => console.error("Error fetching products:", error));
   }, []);
 
   return (
     <div className="product-container">
+      <div className="productcountpart">  
+        <label><b>Newly Arrivals: {count}</b></label>
+      </div>
+
       <h2 className="product-title">Featured Products</h2>
       <div className="product-grid">
         {products.map(product => (
           <div className="product-card" key={product.id}>
-            <img className="product-image" src={product.image} alt={product.title} />
+            <img 
+              className="product-image" 
+              src={product.image} 
+              alt={product.title} 
+            />
             <h4 className="product-name">{product.title}</h4>
             <p className="product-price">${product.price}</p>
           </div>
